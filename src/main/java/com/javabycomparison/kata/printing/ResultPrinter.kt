046422d9c84950c255c4d2752d8ac91b8ef3dffb
danielrotenberg <danirg10000@gmail.com>
2025-03-19 15:21:34 +0200

    * Refactoring ResultPrinter, WIP

--- src/main/java/com/javabycomparison/kata/printing/ResultPrinter.kt
+++ src/main/java/com/javabycomparison/kata/printing/ResultPrinter.kt
@@ -57,55 +57,78 @@ object ResultPrinter {
                     "",
                     Collections.nCopies<kotlin.String?>(
                         max(
-                            (calculateLOCLength(r1, r2) - LOC.length).toDouble(),
+                            (calculateLOCLength(r1.LOC, r2.LOC) - LOC.length).toDouble(),
                             0.0
                         ).toInt(), " "
                     )
                 )
             )
             .append(LOC)
-        stringBuilderForFirstResult.append(rdp.printLOC(r1, calculateLOCLength(r1, r2)))
-        stringBuilderForSecondResult.append(rdp.printLOC(r2, calculateLOCLength(r1, r2)))
+        stringBuilderForFirstResult.append(rdp.printLOC(r1, calculateLOCLength(r1.LOC, r2.LOC)))
+        stringBuilderForSecondResult.append(rdp.printLOC(r2, calculateLOCLength(r1.LOC, r2.LOC)))
         stringBuilderForHeader
             .append(
                 String.join(
                     "",
                     Collections.nCopies<kotlin.String?>(
-                        max((calculateCommentLOCLength(r1, r2) - COMMENT_LOC.length).toDouble(), 0.0).toInt(), " "
+                        max(
+                            (calculateCommentLOCLength(r1.commentLOC, r2.commentLOC) - COMMENT_LOC.length).toDouble(),
+                            0.0
+                        ).toInt(), " "
                     )
                 )
             )
             .append(COMMENT_LOC)
-        stringBuilderForFirstResult.append(rdp.printCommentLOC(r1, calculateCommentLOCLength(r1, r2)))
-        stringBuilderForSecondResult.append(rdp.printCommentLOC(r2, calculateCommentLOCLength(r1, r2)))
+        stringBuilderForFirstResult.append(
+            rdp.printCommentLOC(
+                r1, calculateCommentLOCLength(
+                    r1.commentLOC,
+                    r2.commentLOC
+                )
+            )
+        )
+        stringBuilderForSecondResult.append(
+            rdp.printCommentLOC(
+                r2, calculateCommentLOCLength(
+                    r1.commentLOC,
+                    r2.commentLOC
+                )
+            )
+        )
         stringBuilderForHeader
             .append(
                 String.join(
                     "",
                     Collections.nCopies<kotlin.String?>(
-                        max((calculateNumMethodsLength(r1, r2) - NUM_METHODS.length).toDouble(), 0.0).toInt(), " "
+                        max(
+                            (calculateNumMethodsLength(r1.numMethod, r2.numMethod) - NUM_METHODS.length).toDouble(),
+                            0.0
+                        ).toInt(), " "
                     )
                 )
             )
             .append(NUM_METHODS)
         stringBuilderForFirstResult.append(
-            rdp.printNumMethodLOC(r1, calculateNumMethodsLength(r1, r2))
+            rdp.printNumMethodLOC(r1, calculateNumMethodsLength(r1.numMethod, r2.numMethod))
         )
         stringBuilderForSecondResult.append(
-            rdp.printNumMethodLOC(r2, calculateNumMethodsLength(r1, r2))
+            rdp.printNumMethodLOC(r2, calculateNumMethodsLength(r1.numMethod, r2.numMethod))
         )
         stringBuilderForHeader
             .append(
                 String.join(
                     "",
                     Collections.nCopies<kotlin.String?>(
-                        max((calculateNImportsLength(r1, r2) - N_IMPORTS.length).toDouble(), 0.0).toInt(), " "
+                        max(
+                            (calculateNImportsLength(r1.nImports, r2.nImports) - N_IMPORTS.length).toDouble(),
+                            0.0
+                        ).toInt(), " "
                     )
                 )
             )
             .append(N_IMPORTS)
-        stringBuilderForFirstResult.append(rdp.printNImportsLOC(r1, calculateNImportsLength(r1, r2)))
-        stringBuilderForSecondResult.append(rdp.printNImportsLOC(r2, calculateNImportsLength(r1, r2)))
+        stringBuilderForFirstResult.append(rdp.printNImportsLOC(r1, calculateNImportsLength(r1.nImports, r2.nImports)))
+        stringBuilderForSecondResult.append(rdp.printNImportsLOC(r2, calculateNImportsLength(r1.nImports, r2.nImports)))
         println(stringBuilderForHeader.toString())
         println(stringBuilderForFirstResult.toString())
         println(stringBuilderForSecondResult.toString())
@@ -120,41 +143,39 @@ object ResultPrinter {
     }
 
     private fun calculateLanguageLength(r1: ResultData, r2: ResultData): Int {
-        val languageR1 = if ((r1.type == 0) == true) "Java" else "Python"
-        val languageR2 = if ((r2.type == 0) == true) "Java" else "Python"
+        fun detectLanguageType(type: Int): kotlin.String = if ((type == 0) == true) "Java" else "Python"
 
+        val languageR1 = detectLanguageType(r1.type)
+        val languageR2 = detectLanguageType(r2.type)
+
+        return longestLengthOf(languageR1.length,languageR2.length,LANGUAGE)
         // returns the length of the longest string of the three
-        return max(max(languageR1.length.toDouble(), languageR2.length.toDouble()), LANGUAGE.length.toDouble()).toInt()
     }
 
-    private fun calculateLOCLength(r1: ResultData, r2: ResultData): Int {
+    private fun calculateLOCLength(LOC1: Int, LOC2: Int): Int =
         // returns the length of the longest string of the three
-        return max(
-            max(r1.LOC.toString().length.toDouble(), r2.LOC.toString().length.toDouble()), LOC.length.toDouble()
-        ).toInt()
-    }
+        longestLengthOf(LOC1, LOC2,LOC)
 
-    private fun calculateCommentLOCLength(r1: ResultData, r2: ResultData): Int {
+    private fun calculateCommentLOCLength(commentLOC1: Int, commentLOC2: Int): Int =
         // returns the length of the longest string of the three
-        return max(
-            max(r1.commentLOC.toString().length.toDouble(), r2.commentLOC.toString().length.toDouble()),
-            COMMENT_LOC.length.toDouble()
-        ).toInt()
-    }
+        longestLengthOf(commentLOC1, commentLOC2, COMMENT_LOC)
 
-    private fun calculateNumMethodsLength(r1: ResultData, r2: ResultData): Int {
+    private fun calculateNumMethodsLength(numMethod1: Int, numMethod2: Int): Int =
         // returns the length of the longest string of the three
-        return max(
-            max(r1.numMethod.toString().length.toDouble(), r2.numMethod.toString().length.toDouble()),
-            NUM_METHODS.length.toDouble()
-        ).toInt()
-    }
+        longestLengthOf(numMethod1, numMethod2, NUM_METHODS)
 
-    private fun calculateNImportsLength(r1: ResultData, r2: ResultData): Int {
+    private fun calculateNImportsLength(nImports1: Int, nImports2: Int): Int =
         // returns the length of the longest string of the three
+        longestLengthOf(nImports1, nImports2, N_IMPORTS)
+
+    private fun longestLengthOf(
+        nImports1: Int,
+        nImports2: Int,
+        type: kotlin.String
+    ): Int {
         return max(
-            max(r1.nImports.toString().length.toDouble(), r2.nImports.toString().length.toDouble()),
-            N_IMPORTS.length.toDouble()
+            max(nImports1.toString().length.toDouble(), nImports2.toString().length.toDouble()),
+            type.length.toDouble()
         ).toInt()
     }
 }
